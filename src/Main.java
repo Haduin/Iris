a8f6dc0549571a04c009b6aa4fae98c122f2e5c3
simplycodestudio <simplycodestudio@gmail.com>
2021-03-21 21:57:34 +0100

    second

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,49 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.sql.Array;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class Main {
 
+    static Map<Integer,List<Iris>> testColumnMap = new HashMap<>();
+
     public static void main(String[] args) throws FileNotFoundException {
 
         List<Iris> list = new ArrayList<>();
 
-        Scanner scanner = new Scanner(new File("C:\\Users\\Cuba\\Desktop\\studia\\IV\\NAI\\cwiczenia\\2\\algor\\iris_training.txt"));
+        Scanner scanner = new Scanner(new File("C:\\Users\\48505\\Desktop\\Iris\\iris_training.txt"));
 
-        while(scanner.hasNext()){
+        int pilot = 0;
+        while (scanner.hasNext()) {
             String[] split = scanner.nextLine().split(" ");
-            Iris iris = new Iris(split[0].replace(',','.'),split[1].replace(',','.'),split[2].replace(',','.'),split[3].replace(',','.'),split[4]);
-            list.add(iris);
-//            list.add(new Iris(Double.parseDouble(split[0]), Double.parseDouble(split[1]), Double.parseDouble(split[2]), Double.parseDouble(split[3])));
+
+
+            if (pilot==0){
+            for (int i=0;i<split.length;i++){
+                testColumnMap.put(i+1, new ArrayList<>());
+            }}
+
+            for (int i=0;i<split.length;i++){
+                testColumnMap.get(i+1).add(new Iris(split[i]));
+            }
+
+            pilot++;
         }
 
         list.stream().forEach(System.out::println);
 
+//
+//        Double min = Collections.min(list.stream().map(iris -> Double.parseDouble(iris.getT1())).collect(Collectors.toList()));
+//        Double max = Collections.max(list.stream().map(iris -> Double.parseDouble(iris.getT1())).collect(Collectors.toList()));
+//
+//        System.out.println(min + " " + max);
+//
+//        list.stream().forEach(iris -> System.out.println((Double.parseDouble(iris.getT1()) - min) / (max - min))
+//
+//        );
 
-
-        Double min = Collections.min(list.stream().map(iris -> Double.parseDouble(iris.getT1())).collect(Collectors.toList()));
-        Double max = Collections.max(list.stream().map(iris -> Double.parseDouble(iris.getT1())).collect(Collectors.toList()));
-
-        System.out.println(min+" " + max);
-
-        list.stream().forEach(iris -> System.out.println((Double.parseDouble(iris.getT1())-min)/(max-min))
-
-        );
-
-        scanner.close();
+//        scanner.close();
     }
 }
---
+++ src/TestMinMax.java
@@ -0,0 +1,24 @@
+public class TestMinMax {
+
+   private int min;
+   private int max;
+   private String name;
+
+    public TestMinMax(int min, int max, String name) {
+        this.min = min;
+        this.max = max;
+        this.name = name;
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+    public int getMax() {
+        return max;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
